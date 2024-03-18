76fa89255b1496f8d682d3e98e287ef0e426a9bc
Nikola R. Hristov <Nikola@Playform.Cloud>
2024-03-18 15:07:11 +0200

    squash!

--- Source/Class/TypeScriptDocument.ts
+++ Source/Class/TypeScriptDocument.ts
@@ -7,7 +7,7 @@
 export default new (await import("commander")).Command()
 	.name("TypeScriptDocument")
 	.version(process.env["VERSION_PACKAGE"] ?? "0.0.1")
-	.description("📃 Document TypeScript")
+	.description("📃 Document TypeScript.")
 	.argument("<File...>", "Document File.")
 	.action((await import("../Function/Document.js")).default)
 	.parse();
