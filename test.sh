ead103ae9f989ca872d6c3fdf8d6f559e15a5d7e
Nikola R. Hristov <Nikola@Playform.Cloud>
2024-03-19 15:26:10 +0200

    squash!

--- test.sh
+++ test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-typedoc \
+\typedoc \
 	--commentStyle all \
-	# --gitRevision Current \
+	--gitRevision Current \
 	--customCss /d/Developer/Application/Playform/NPM/TypeScriptDocument/Target/Stylesheet/Theme.css \
 	--includeVersion \
 	--out ./Documentation \
-	# --plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/Target/Variable/Load.js \
-	# --plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/node_modules/typedoc-plugin-remove-references/dist/index.js \
-	# --plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/node_modules/typedoc-plugin-rename-defaults/index.js \
-	# --plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/node_modules/typedoc-plugin-mdn-links/dist/index.js \
-	# --plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/node_modules/typedoc-plugin-zod/dist/plugin.js \
-	# --plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/node_modules/typedoc-plugin-merge-modules/dist/index.js \
-	# --plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/node_modules/typedoc-plugin-keywords/index.js \
+	--plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/Target/Variable/Load.js \
+	--plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/node_modules/typedoc-plugin-remove-references/dist/index.js \
+	--plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/node_modules/typedoc-plugin-rename-defaults/index.js \
+	--plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/node_modules/typedoc-plugin-mdn-links/dist/index.js \
+	--plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/node_modules/typedoc-plugin-zod/dist/plugin.js \
+	--plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/node_modules/typedoc-plugin-merge-modules/dist/index.js \
+	--plugin /d/Developer/Application/Playform/NPM/TypeScriptDocument/node_modules/typedoc-plugin-keywords/index.js \
 	--searchInComments \
 	--keywords documentation \
 	--keywords javascript \
@@ -33,5 +33,3 @@ typedoc \
 	--entryPoints Source/Interface/Build.ts \
 	--entryPoints Source/Class/TypeScriptDocument.ts \
 	--entryPoints Source/Class/Theme.ts
-
-
