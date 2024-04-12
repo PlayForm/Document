9c3e50f9b421b3ef7aac6ecacbb3d7f82a3e4a1c
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-12 23:59:02 +0300

    squash!

--- Source/Class/Document.ts
+++ Source/Class/Document.ts
@@ -7,7 +7,7 @@
 export default new (await import("commander")).Command()
 	.name("Document")
 	.version(process.env["VERSION_PACKAGE"] ?? "0.0.1")
-	.description("📃 Document.")
+	.description("📃 Document")
 	.argument("<File...>", "File.")
 	.action((await import("@Function/Document.js")).default)
 	.parse();
