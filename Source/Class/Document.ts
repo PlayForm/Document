5bf5b39d2230f9cf6ecc17b9db2215685f2f1c4c
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-22 17:10:38 +0300

    squash!

--- Source/Class/Document.ts
+++ Source/Class/Document.ts
@@ -8,6 +8,6 @@ export default new (await import("commander")).Command()
 	.name("Document")
 	.version(process.env["VERSION_PACKAGE"] ?? "0.0.1")
 	.description("📃 Document —")
-	.argument("<File...>", "📂 File —")
+	.argument("<File...>", "📝 File —")
 	.action((await import("@Function/Document.js")).default)
 	.parse();
