fe3b2de76b38b082bcd934a6e43818d80417095c
Nikola Hristov <Nikola@PlayForm.Cloud>
2026-01-04 12:53:49 +0200

    save
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Target/Function/Document.d.ts
+++ Target/Function/Document.d.ts
@@ -6,6 +6,6 @@ import type Interface from "../Interface/Document.js";
 declare const _default: Interface;
 export default _default;
 export declare const Exec: import("../Interface/Exec.js").default;
-export declare const resolve: (...paths: string[]) => string;
+export declare const resolve: typeof import("node:path").resolve;
 export declare const Pipe: string[];
 export declare const Current: string;
